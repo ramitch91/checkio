9b93e41cc228d1ed13f065fd1a0960c72813c496
Ricky <ramitch91@gmail.com>
2022-09-04 06:04:56 -0500

    add solution for nearest_value.py

--- solutions/Initiation/nearest_value.py
+++ solutions/Initiation/nearest_value.py
@@ -4,11 +4,16 @@
 # 
 # You are given a list of values as set form and a value for which you need to find the nearest one.
 # 
-# For example, we have the following set of numbers: 4, 7, 10, 11, 12, 17, and we need to find the nearest value to the number 9. If we sort this set in the ascending order, then to the left of number 9 will be number 7 and to the right - will be number 10. But 10 is closer than 7, which means that the correct answer is 10.
+# For example, we have the following set of numbers: 4, 7, 10, 11, 12, 17, and we need to find the nearest value
+# to the number 9. If we sort this set in the ascending order, then to the left of number 9 will be number 7 and
+# to the right - will be number 10. But 10 is closer than 7, which means that the correct answer is 10.
 # 
 # A few clarifications:
 # 
-# If 2 numbers are at the same distance, you need to choose the smallest one;The set of numbers is always non-empty, i.e. the size is >=1;The given value can be in this set, which means that it’s the answer;The set can contain both positive and negative numbers, but they are always integers;The set isn’t sorted and consists of unique numbers.Input:Two arguments. A list of values in the set form. The sought value is an int.
+# If 2 numbers are at the same distance, you need to choose the smallest one;The set of numbers is always non-empty,
+# i.e. the size is >=1;The given value can be in this set, which means that it’s the answer;The set can contain both
+# positive and negative numbers, but they are always integers;The set isn’t sorted and consists of unique numbers.
+# Input:Two arguments. A list of values in the set form. The sought value is an int.
 # 
 # Output:Int.
 # 
@@ -17,7 +22,15 @@
 
 def nearest_value(values: set, one: int) -> int:
     # your code here
-    return None
+    closest_difference = 1000
+    sorted_values = sorted(values)
+    for value in sorted_values:
+        difference = abs(one-value)
+        if difference < closest_difference:
+            closest_difference = difference
+            nearest = value
+
+    return nearest
 
 
 if __name__ == '__main__':
