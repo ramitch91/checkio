9a74271a4eb538d090e4559e7ee179dd361205ae
Ricky <ramitch91@gmail.com>
2022-09-05 07:06:45 -0500

    add solution for popular_words.py

--- solutions/Home/popular_words.py
+++ solutions/Home/popular_words.py
@@ -2,23 +2,36 @@
 
 # In this mission your task is to determine the popularity of certain words in the text.
 # 
-# At the input of your function are given 2 arguments: the text and the array of words the popularity of which you need to determine.
+# At the input of your function are given 2 arguments: the text and the array of words the popularity of which you
+# need to determine.
 # 
 # When solving this task pay attention to the following points:
 # 
-# The words should be sought in all registers. This means that if you need to find a word "one" then words like "one", "One", "oNe", "ONE" etc. will do.The search words are always indicated in the lowercase.If the word wasn’t found even once, it has to be returned in the dictionary with  0 (zero) value.Input:The text and the search words array.
+# The words should be sought in all registers. This means that if you need to find a word "one" then words like "one",
+# "One", "oNe", "ONE" etc. will do.The search words are always indicated in the lowercase.If the word wasn’t found
+# even once, it has to be returned in the dictionary with  0 (zero) value.Input:The text and the search words array.
 # 
-# Output:The dictionary where the search words are the keys and values are the number of times when those words are occurring in a given text.
+# Output:The dictionary where the search words are the keys and values are the number of times when those words are
+# occurring in a given text.
 # 
 # Precondition:
-# The input text will consists of English letters in uppercase and lowercase and whitespaces.
+# The input text will consist of English letters in uppercase and lowercase and whitespaces.
 # 
 # 
 # END_DESC
 
 def popular_words(text: str, words: list) -> dict:
     # your code here
-    return None
+    text = " ".join(text.splitlines())
+    text_list = text.lower().split(" ")
+    print(text_list)
+    popular_dict = {}
+    for word in words:
+        if word not in text_list:
+            popular_dict[word] = 0
+        else:
+            popular_dict[word] = text_list.count(word)
+    return popular_dict
 
 
 if __name__ == '__main__':
