fdd022dabd2f799e3988771085740fa89ef0179f
Ricky <ramitch91@gmail.com>
2022-09-05 06:41:06 -0500

    add solution for between_markers.py

--- solutions/Home/between_markers.py
+++ solutions/Home/between_markers.py
@@ -1,8 +1,13 @@
 #!/home/ricky/programs/python/checkio/venv/bin/checkio --domain=py run between-markers
 
-# You are given a string and two markers (the initial and final). You have to find a substring enclosed between these two markers. But there are a few important conditions:
+# You are given a string and two markers (the initial and final). You have to find a substring enclosed between
+# these two markers. But there are a few important conditions:
 # 
-# The initial and final markers are always different.If there is no initial marker, then the first character should be considered the beginning of a string.If there is no final marker, then the last character should be considered the ending of a string.If the initial and final markers are missing then simply return the whole string.If the final marker comes before the initial marker, then return an empty string.Input:Three arguments. All of them are strings. The second and third arguments are the initial and final markers.
+# The initial and final markers are always different.If there is no initial marker, then the first character should be
+# considered the beginning of a string.If there is no final marker, then the last character should be considered
+# the ending of a string.If the initial and final markers are missing then simply return the whole string.
+# If the final marker comes before the initial marker, then return an empty string.Input:Three arguments.
+# All of them are strings. The second and third arguments are the initial and final markers.
 # 
 # Output:A string.
 # 
@@ -15,8 +20,26 @@ def between_markers(text: str, begin: str, end: str) -> str:
     """
         returns substring between two given markers
     """
+
     # your code here
-    return ''
+
+    if begin not in text and end not in text:
+        return text
+
+    begin_index = text.find(begin)
+    end_index = text.find(end)
+
+    if begin in text and len(begin) > 1:
+        begin_index = begin_index + len(begin) - 1
+
+    if end_index == -1:
+        end_index = 1000
+
+    if end_index < begin_index:
+        begin_index, end_index = end_index, begin_index
+        return ""
+
+    return text[begin_index + 1: end_index]
 
 
 if __name__ == '__main__':
