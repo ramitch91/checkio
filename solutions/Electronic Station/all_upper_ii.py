50fef4f467d28855453a373e009df07aeddb88b7
Ricky <ramitch91@gmail.com>
2022-10-08 20:36:46 -0500

    add solution for all_upper_ii.py

--- solutions/Electronic Station/all_upper_ii.py
+++ solutions/Electronic Station/all_upper_ii.py
@@ -1,6 +1,7 @@
 #!/home/ricky/programs/python/checkio/venv/bin/checkio --domain=py run all-upper-ii
 
-# Check if a given string has all symbols in upper case. If the string is empty or doesn't have any letter in it - function should return False.
+# Check if a given string has all symbols in upper case. If the string is empty or doesn't have any letter in it -
+# function should return False.
 # 
 # Input:A string.
 # 
@@ -13,7 +14,17 @@
 
 def is_all_upper(text: str) -> bool:
     # your code here
-    return False
+    if not any(c.isalpha() for c in text):
+        return False
+    else:
+        for letter in text:
+            if letter.isalpha():
+                new_text = text.split()
+        for word in new_text:
+            new_word = word.upper()
+            if word != new_word:
+                return False
+    return True
 
 
 if __name__ == "__main__":
