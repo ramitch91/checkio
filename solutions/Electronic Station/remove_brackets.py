ad7d47e88248140e18a318bcc2886384b9b473c3
Ricky <ramitch91@gmail.com>
2022-11-11 14:11:07 -0600

    modify instruction set for remove_brackets.py

--- solutions/Electronic Station/remove_brackets.py
+++ solutions/Electronic Station/remove_brackets.py
@@ -2,13 +2,19 @@
 
 # Before solving this mission, you can try to solve the"Brackets"mission.
 # 
-# Your task is to restore the balance of open and closed brackets by removing the unnecessary ones,     while trying to use the minimum number of deletions.
+# Your task is to restore the balance of open and closed brackets by removing the unnecessary ones,
+# while trying to use the minimum number of deletions.
 # 
 # Only 3 types of brackets (), [] and {} can be used in the given string.
 # 
-# Only a parenthesis can close a parenthesis. That is, in this expression "(}" - the brackets aren’t balanced.     In an empty string, i.e., in a string that doesn’t contain any brackets - the brackets are balanced,     but removing all of the brackets isn’t considered to be an optimal solution.
+# Only a parenthesis can close a parenthesis. That is, in this expression "(}" - the brackets aren’t balanced.
+# In an empty string, i.e., in a string that does not contain any brackets - the brackets are balanced,
+# but removing all of the brackets isn’t considered to be an optimal solution.
 # 
-# If there are more than one correct answer,     then you should choose the one where the character that can be removed is closer to the beginning.     For example, in this case  "[(])",  the correct answer will be "()",     since the removable brackets are closer to the beginning of the line.
+# If there are more than one correct answer,
+# then you should choose the one where the character that can be removed is closer to the beginning.
+# For example, in this case  "[(])",  the correct answer will be "()",
+# since the removable brackets are closer to the beginning of the line.
 # 
 # Input:A string of characters () {} []
 # 
